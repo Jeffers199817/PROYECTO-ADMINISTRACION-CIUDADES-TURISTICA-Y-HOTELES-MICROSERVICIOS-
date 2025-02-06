21687499390fce4bb2cce89d25ded4618f327165
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-06 13:35:38 -0500

    Implementación de la api de cities finalizada

---
+++ cities-service/src/main/java/com/milenyumsoft/cities_service/controller/CitieController.java
@@ -0,0 +1,28 @@
+package com.milenyumsoft.cities_service.controller;
+
+import com.milenyumsoft.cities_service.modelo.Citie;
+import com.milenyumsoft.cities_service.service.ICitieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/cities")
+public class CitieController {
+
+    @Autowired
+    private ICitieService citieServ;
+
+
+    @GetMapping("/{name}/{country}")
+    public Citie getCitie(@PathVariable("name") String name, @PathVariable("country") String country){
+
+        return  citieServ.getCitie(name, country);;
+    }
+
+
+
+}
--- cities-service/src/main/java/com/milenyumsoft/cities_service/service/ICitieService.java
+++ cities-service/src/main/java/com/milenyumsoft/cities_service/service/ICitieService.java
@@ -1,9 +1,9 @@
 package com.milenyumsoft.cities_service.service;
 
 import com.milenyumsoft.cities_service.modelo.Citie;
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public interface ICitieService {
 
     public Citie getCitie(String name, String country);
