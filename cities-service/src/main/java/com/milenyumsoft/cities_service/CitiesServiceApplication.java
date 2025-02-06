0df263889b6fe908d54837344272e71d17cde39b
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-06 10:17:25 -0500

    3. Integración del servicio cities-service

---
+++ cities-service/src/main/java/com/milenyumsoft/cities_service/CitiesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.milenyumsoft.cities_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesServiceApplication.class, args);
+	}
+
+}
