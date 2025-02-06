0586d26356b4e40469bacc85b3339413416e16a0
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-06 10:16:43 -0500

    2. creación del microservicio hotles-service

---
+++ hotels-service/src/main/java/com/milenyumsoft/hotels_service/HotelsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.milenyumsoft.hotels_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsServiceApplication.class, args);
+	}
+
+}
